18ab18a10c60a7f628e8d06683a0de7a4cbde322
Sygzyl <w.n.earl@gmail.com>
2024-10-20 12:16:51 +0200

    Changes made as per issue-2 to helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,12 @@
 # Request name from user as variable
-user_name = input("Please enter your first name: \n")
-# Print out name of user
-print(f"Your name is: {user_name}") 
+user_name = input("Please input your name: \n")
 # Request age from user as variable
-user_age = input("Please enter your age: \n") 
-# Print out age of user
-print(f"Your age is: {user_age}") 
+user_age = input("Please input your age: \n")
+# Request house number from user
+house_number = input("Please input your house number: \n")
+# Request street name from user
+street_name = input("Please input your street name: \n")
+
+# Print out above user input in readable format
+print(f'''This is {user_name}. Their age is {user_age} 
+      and they live at house number {house_number} on {street_name}''')
