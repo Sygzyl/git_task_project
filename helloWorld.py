9ad25c5bc4c6dd08d6d836025e827de5d49cb86f
Sygzyl <w.n.earl@gmail.com>
2024-10-20 11:32:39 +0200

    Committing changes to helloWorld.py from issues-1

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,4 @@
-print("Git is awesome!") 
+user_name = input("Please enter your first name: \n")
+print(f"Your name is: {user_name}") 
+user_age = input("Please enter your age: \n") 
+print(f"Your age is: {user_age}") 
