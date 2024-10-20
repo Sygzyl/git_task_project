612328e018509ea120fd2b9d35cc5c91b30c43fc
Sygzyl <w.n.earl@gmail.com>
2024-10-20 12:03:53 +0200

    Comments added as requested in issue#1

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,8 @@
+# Request name from user as variable
 user_name = input("Please enter your first name: \n")
+# Print out name of user
 print(f"Your name is: {user_name}") 
+# Request age from user as variable
 user_age = input("Please enter your age: \n") 
+# Print out age of user
 print(f"Your age is: {user_age}") 
