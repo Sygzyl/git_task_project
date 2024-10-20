74aa6a241c9f18af2927d9ae217808bd16104365
Sygzyl <w.n.earl@gmail.com>
2024-10-20 11:41:06 +0200

    fixing stuff

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,1 @@
-user_name = input("Please enter your first name: \n")
-print(f"Your name is: {user_name}") 
-user_age = input("Please enter your age: \n") 
-print(f"Your age is: {user_age}") 
+print("Hello World!")
