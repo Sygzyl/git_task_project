f75d922051cbcf45afa44e1530cc3479ce63bc65
Sygzyl <w.n.earl@gmail.com>
2024-10-20 11:43:58 +0200

    Committing updates to helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+user_name = input("Please enter your first name: \n")
+print(f"Your name is: {user_name}") 
+user_age = input("Please enter your age: \n") 
+print(f"Your age is: {user_age}") 
